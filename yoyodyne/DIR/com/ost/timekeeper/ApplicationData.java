4ab0998e9ee9d3ac386d3af3b0af76afbb27b937
Davide Cavestro <davide.cavestro@gmail.com>
2005-01-23 11:54:03 +0100

    numero versione

--- yoyodyne/DIR/com/ost/timekeeper/ApplicationData.java
+++ yoyodyne/DIR/com/ost/timekeeper/ApplicationData.java
@@ -6,6 +6,7 @@
 
 package com.ost.timekeeper;
 
+import com.ost.timekeeper.cache.*;
 import com.ost.timekeeper.conf.*;
 import java.io.*;
 import java.util.*;
@@ -14,6 +15,7 @@ import java.util.*;
  * Contiene i dati descrittivi dell'applicazione quali, ad esempio,il numero di versione...
  *
  * @author  davide
+ * @todo completare ampliando le informazioni disponibili.
  */
 public final class ApplicationData {
 
@@ -50,7 +52,7 @@ public final class ApplicationData {
 	/**
 	 * Proprietà di configurazione di questo descrittore.
 	 */
-	private final static Properties _applicationProperties = new Properties ();
+	private final static Properties _releaseProperties = new Properties ();
 	
 	/**
 	 * Inizializzatore.
@@ -60,10 +62,10 @@ public final class ApplicationData {
 			/*
 			 * carica dati di configurazione.
 			 */
-			_applicationProperties.load(ApplicationData.class.getResourceAsStream("application.properties"));
+			_releaseProperties.load(ApplicationData.class.getResourceAsStream("release.properties"));
 		} catch (IOException ioe) {
 			//throw new RuntimeException (ioe);
-			Application.getLogger ().warning ("Missing application properties file.");
+			Application.getLogger ().warning ("Missing release properties file.");
 		}
 	}
 	
@@ -85,8 +87,61 @@ public final class ApplicationData {
 	 *
 	 * @return il nome interno dell'applicazione.
 	 */
-	public static String getApplicationInternalName (){
-		return SettingsSupport.getStringProperty (_applicationProperties, PROPNAME_INTERNALAPPLICATIONNAME);
+	public String getApplicationInternalName (){
+		return SettingsSupport.getStringProperty (_releaseProperties, PROPNAME_INTERNALAPPLICATIONNAME);
+	}
+	
+
+	/**
+	 * Il numero di versione (cache).
+	 */
+	private DisposableData _versionNumber;
+	/**
+	 * Ritorna il numero di versione di questo rilascio.
+	 *
+	 * @return il numero di versione di questo rilascio.
+	 */	
+	public final String getVersionNumber (){
+		if (_versionNumber==null || !_versionNumber.isValid ()){
+			final StringBuffer sb = new StringBuffer ();
+			sb.append (_releaseProperties.getProperty (PROPNAME_MAJORVERSIONNUMBER));
+			sb.append (".");
+			sb.append (_releaseProperties.getProperty (PROPNAME_MINORVERSIONNUMBER));
+			_versionNumber = new DisposableData (sb.toString ());
+		}
+		return (String)_versionNumber.getData ();
+	}
+	
+	/**
+	 * Il numero di build (cache).
+	 */
+	private DisposableData _buildNumber;
+	/**
+	 * Ritorna il numero di build di questo rilascio.
+	 *
+	 * @return il numero di build di questo rilascio.
+	 */	
+	public final String getBuildNumber (){
+		if (_buildNumber==null || !_buildNumber.isValid ()){
+			_buildNumber = new DisposableData (_releaseProperties.getProperty (PROPNAME_BUILDNUMBER));
+		}
+		return (String)_buildNumber.getData ();
+	}
+	
+	/**
+	 * La data di rilascio (cache).
+	 */
+	private DisposableData _releaseDate;
+	/**
+	 * Ritorna la data di rilascio.
+	 *
+	 * @return la data di rilascio.
+	 */	
+	public final Calendar getReleaseDate (){
+		if (_versionNumber==null || !_versionNumber.isValid ()){
+			_versionNumber = new DisposableData (SettingsSupport.getCalendarProperty (_releaseProperties, PROPNAME_RELEASE_DATE));
+		}
+		return (Calendar)_versionNumber.getData ();
 	}
 	
 }
