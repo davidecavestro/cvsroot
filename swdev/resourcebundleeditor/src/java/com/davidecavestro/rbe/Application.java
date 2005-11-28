bcdb1c26da0e6156bc306e45e336b755cf17c643
Davide Cavestro <davide.cavestro@gmail.com>
2005-11-28 22:07:21 +0100

    *** empty log message ***

---
+++ swdev/resourcebundleeditor/src/java/com/davidecavestro/rbe/Application.java
@@ -0,0 +1,19 @@
+/*
+ * Application.java
+ *
+ * Created on 26 novembre 2005, 14.55
+ */
+
+package com.davidecavestro.rbe;
+
+/**
+ *
+ * @author  davide
+ */
+public class Application {
+	
+	/** Creates a new instance of Application */
+	public Application () {
+	}
+	
+}
---
+++ swdev/resourcebundleeditor/src/java/com/davidecavestro/rbe/Launcher.java
@@ -0,0 +1,28 @@
+/*
+ * Launcher.java
+ *
+ * Created on 26 novembre 2005, 14.55
+ */
+
+package com.davidecavestro.rbe;
+
+import com.davidecavestro.rbe.gui.MainWIndow;
+
+/**
+ *
+ * @author  davide
+ */
+public class Launcher {
+	
+	/** Creates a new instance of Launcher */
+	public Launcher () {
+	}
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main (String[] args) {
+		new MainWIndow ().show ();
+	}
+	
+}
