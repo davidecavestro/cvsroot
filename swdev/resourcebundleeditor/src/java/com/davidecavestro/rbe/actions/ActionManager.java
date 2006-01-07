02a21861e12695f832f12b0638c124735d69e249
Davide Cavestro <davide.cavestro@gmail.com>
2006-01-07 19:45:44 +0100

    spostati da "gui"

---
+++ swdev/resourcebundleeditor/src/java/com/davidecavestro/rbe/actions/ActionManager.java
@@ -0,0 +1,32 @@
+/*
+ * ActionManager.java
+ *
+ * Created on 31 dicembre 2005, 11.11
+ */
+
+package com.davidecavestro.rbe.actions;
+
+import com.davidecavestro.rbe.ApplicationContext;
+
+/**
+ * Gestore delle Action.
+ *
+ * @author  davide
+ */
+public class ActionManager {
+	
+//	private final ApplicationContext _context;
+	
+	/** Costruttore. */
+	public ActionManager () {
+//		this._context = context;
+	}
+	
+	private FindNextAction _findNextAction;
+	public FindNextAction getFindNextAction (){
+		if (null==this._findNextAction){
+			this._findNextAction = new FindNextAction ();
+		}
+		return this._findNextAction;
+	}
+}
