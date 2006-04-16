e9289368aa6c99d75f34448459321831e8069d27
Davide Cavestro <davide.cavestro@gmail.com>
2006-04-16 10:40:24 +0200

    JavaDoc

--- swdev/resourcebundleeditor/src/java/com/davidecavestro/rbe/gui/search/SearchNavigator.java
+++ swdev/resourcebundleeditor/src/java/com/davidecavestro/rbe/gui/search/SearchNavigator.java
@@ -7,10 +7,17 @@
 package com.davidecavestro.rbe.gui.search;
 
 /**
+ * Naviga nei risultati di una ricerca.
  *
  * @author  davide
  */
 public interface SearchNavigator {
+	/**
+	 * Passa al risultato successivo.
+	 */
 	void nextMatch (Matcher m);
+	/**
+	 * Torna al risultato precedente.
+	 */
 	void previousMatch (Matcher m);
 }
